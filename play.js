f950328b97a90be59c57e63a149432e5c31a962b
Andrei Chirita <adchir95@gmail.com>
2022-09-28 18:20:53 +0000

    Exporting the function from a new file client.js and importing it in play.js

--- play.js
+++ play.js
@@ -1,18 +1,4 @@
-const net = require("net");
+const connection = require("./client");
 
-const connection = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-  
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 console.log("Connecting ...");
 connection();
