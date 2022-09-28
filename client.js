1007385599355c27f7e36173dbb58717714d1675
Andrei Chirita <adchir95@gmail.com>
2022-09-28 20:02:11 +0000

    adding constants to their own constant.js file

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
+    IP,
+    PORT
   });
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: Andrei");
-//    conn.write("Move: up");
   });
   conn.on('data', (data) => {
     console.log(data);
